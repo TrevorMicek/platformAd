bf3354c7ae1234147cee2eef59f413d45f789317
TrevorMicek <miceking@comcast.net>
2021-12-17 12:45:12 -0700

    took out correct redux

--- src/pages/store.js
+++ src/pages/store.js
@@ -31,8 +31,20 @@ const App = () => {
 		// fetchCollection,
 	} = useShopify()
 
+    const [cart, setCart] = useState([])
+    const [cartAmount, setCartAmount] = useState(0)
+    const [linkUrl, setLinkUrl] = useState('/')
+    const getUrl = (url) => setLinkUrl(url)
+	const addToCart = (cartItem) => setCart([...cart, cartItem])
+    const getCart = () => setCartAmount(cart.length)
 
+    const storage = () => JSON.parse(window.localStorage.getItem('cart')) || {}
+    const createCart = () => {
 
+        addToCart(storage())
+
+        return createComponent(cart, addVariant)
+    }
 	useEffect(() => {
 		createShop()
 		fetchProducts()
@@ -40,15 +52,31 @@ const App = () => {
 
 		// fetchCollection()
 	}, [])
+    const handleClick = (url) => getUrl(url)
+    const ContinueShopping = () => (
+        <HomeButton linkUrl={linkUrl} geturl={getUrl} title="Continue Shopping" onClick={() => handleClick('../../')} />
+    )
+    const CartButton = () => (
+        <HomeButton linkUrl={linkUrl} geturl={getUrl} title="To Store" onClick={() => handleClick('../')} />
+    )
+    const MainHomeButton = () => (
+        <HomeButton linkUrl={linkUrl} geturl={getUrl} title="Main Website" onClick={() => handleClick('/')} />
+    )
 
-
-
+    const switchButton = () => {
+        switch(linkUrl) {
+            case '/':
+                return <HomeButton linkUrl={linkUrl} geturl={getUrl} title="Main Website" onClick={() => handleClick('/')} />
+            case '../../':
+                return <HomeButton linkUrl={linkUrl} geturl={getUrl} title="Continue Shopping" onClick={() => handleClick('../../')} />
+        }
+    }
 
     const CartWrapper = () => (
         <div className="homeButtonWrapper">
+    {switchButton()}
 
-
-    <Cart  title={props.title}  />
+    <Cart create={createCart} title={props.title} homeButton={CartButton} button={getUrl} />
  </div>
     )
 
@@ -56,8 +84,8 @@ const App = () => {
         <>
 
         <Router>
-			<Products path="/store" cart={CartWrapper} title={props.title} getTitle={props.getTitle}   />
-			<ProductView path='/store/products/:productId' cart={CartWrapper} title={props.getTitle}  />
+			<Products path="/store" cart={CartWrapper} title={props.title} getTitle={props.getTitle} homeButton={MainHomeButton}  />
+			<ProductView path='/store/products/:productId' cart={CartWrapper} title={props.getTitle} homeButton={ContinueShopping} button={getUrl} />
 
         </Router>
         </>
