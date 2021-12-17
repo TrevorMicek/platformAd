c37fa5c3b0e962772b0a9aedd471a391485e2e85
TrevorMicek <miceking@comcast.net>
2021-12-17 15:59:23 -0700

    took out products

--- src/storePage/storeComponents/Products.js
+++ src/storePage/storeComponents/Products.js
@@ -1,30 +1,12 @@
-import React, { useState, useEffect } from "react"
-import { Link } from 'gatsby'
-import Product from "./Product"
-import Header from './Header'
-import Layout from '../../components/layout/layout'
-import styles from '../../styles/StorePage/wrapper.css'
+import React, { useState } from "react"
+import { useShopify } from "../hooks"
+
+
 export default (props) => {
 
-
 	return (
-		<Layout title='Custom Ecommerce'>
-			{props.cart()}
-
-				<div className={styles.app}>
-
-		<div className="Products-wrapper">
-
-
-
-			<Product
-				history={props.history}
-				title={props.title}
-				getTitle={props.getTitle}
-			/>
-
+		<div>
+hi
 		</div>
-		</div>
-		</Layout>
 	)
 }
