0cc4a687ee7506dbf23157a6b8ac5514ddeddd28
TrevorMicek <miceking@comcast.net>
2021-12-17 15:49:41 -0700

    taking out store comps

--- src/storePage/storeComponents/ProductviewWrapper.js
+++ src/storePage/storeComponents/ProductviewWrapper.js
@@ -1,16 +1,12 @@
 import React, { useState } from "react"
-import Productview from "./Productview"
-import Header from '../../components/layout/Header'
+import { useShopify } from "../hooks"
+
 
 export default (props) => {
-    const [headerName, setHeaderName] = useState('')
-    const getHeaderName = (title) => setHeaderName(title)
+
 	return (
-		
-		<div className="Products-wrapper">
-			
-			<Productview title={getHeaderName} />
+		<div>
+
 		</div>
-		
 	)
 }
