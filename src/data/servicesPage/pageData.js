0dcdd97c8dcfbe1cd54e15bdc5441332e8cb70de
TrevorMicek <miceking@comcast.net>
2021-12-17 20:51:55 -0700

    adding store as subdomain

--- src/data/servicesPage/pageData.js
+++
@@ -1,5 +0,0 @@
-const data = {
-    title: "Customized Packages",
-    text: "Alongside these benefits, we offer highly customized packages that include everything you need."
-}
-export default data;
