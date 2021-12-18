ce7c511505481c2a8697a8c1420f47bc8785967b
TrevorMicek <miceking@comcast.net>
2021-12-18 16:32:20 -0700

    took out api call

--- src/components/layout/NavBar.js
+++ src/components/layout/NavBar.js
@@ -1,7 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from "gatsby"
-import { Helmet } from "react-helmet"
-import { useShopify } from "../../storePage/hooks"
 
 
 import './navBarStyles.css';
