865e4a305373b994dabdfdefcfcbf7589ab3a033
TrevorMicek <miceking@comcast.net>
2021-12-19 08:12:07 -0700

    took out pricing comps for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,6 @@ import React from "react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
-import Pricing from '../components/tailwindComps/Pricing'
 import FeatureSection from '../components/tailwindComps/FeatureSection'
 import MoreInfo from '../components/tailwindComps/FrontPageInfo'
 const IndexPage = () => (
@@ -10,11 +9,7 @@ const IndexPage = () => (
     <SEO title="Home" />
 <FeatureSection />
 <MoreInfo />
-<Pricing />
-<Pricing />
-<Pricing />
-<Pricing />
-<Pricing />
+
   </Layout>
 )
 export default IndexPage
