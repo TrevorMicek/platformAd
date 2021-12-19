9a5ec6f4d1c23baa2a767c408eedba7617aed693
TrevorMicek <miceking@comcast.net>
2021-12-18 21:27:54 -0700

    adding 5x biggest comp for testing

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 
 import Layout from "../components/layout/layout"
 import SEO from "../components/layout/seo"
-
+import Pricing from '../components/tailwindComps/Pricing'
 import FeatureSection from '../components/tailwindComps/FeatureSection'
 import MoreInfo from '../components/tailwindComps/FrontPageInfo'
 const IndexPage = () => (
@@ -10,7 +10,11 @@ const IndexPage = () => (
     <SEO title="Home" />
 <FeatureSection />
 <MoreInfo />
-
+<Pricing />
+<Pricing />
+<Pricing />
+<Pricing />
+<Pricing />
   </Layout>
 )
 export default IndexPage
