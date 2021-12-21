74aa9e158fa0648a8a5befc5078bc9d8e6202e1f
TrevorMicek <miceking@comcast.net>
2021-12-21 13:52:34 -0700

    fixed nav icon

--- src/components/tailwindComps/SecondaryHeader.js
+++ src/components/tailwindComps/SecondaryHeader.js
@@ -97,7 +97,7 @@ export default function Header(props) {
                 <div className="flex items-center md:hidden">
                   <Popover.Button className="bg-gray-900 rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus-ring-inset focus:ring-white">
                     <span className="sr-only">Open main menu</span>
-                    <MenuIcon className="mb-4 h-8 w-8" aria-hidden="true" />
+                    <MenuIcon className="relative bottom-3 h-7 w-7" aria-hidden="true" />
                   </Popover.Button>
                 </div>
               </div>
