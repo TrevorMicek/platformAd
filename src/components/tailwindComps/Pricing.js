7d70a9919082f150c1d154eeb4a4f80e857ba2ca
TrevorMicek <miceking@comcast.net>
2021-12-21 12:41:16 -0700

    took out extra comparison comp and fixed styles on active button

--- src/components/tailwindComps/Pricing.js
+++ src/components/tailwindComps/Pricing.js
@@ -527,7 +527,7 @@ export default function Pricing() {
             'text-sm font-semibold uppercase tracking-wide'
           )}
         >
-          {plan.title}
+          {plan.title} <br />{plan.pages} pages
         </h3>
         <div className="flex flex-col items-start sm:flex-row sm:items-center sm:justify-between lg:flex-col lg:items-start">
           <div className="mt-3 flex items-center">
@@ -572,7 +572,7 @@ export default function Pricing() {
         )}
       >
         {plan.mainFeatures.map((mainFeature) => (
-          <li key={mainFeature.id} className="py-3 flex items-center">
+          <li key={mainFeature.id} className="py-2 flex items-center">
             <CheckIcon
               className={classNames(
                 plan.featured ? 'text-indigo-500' : 'text-indigo-200',
@@ -624,14 +624,14 @@ export default function Pricing() {
             <button
               type="button"
               onClick={() => setPackages('ecom')}
-              className="relative bg-white py-2 px-6 border-indigo-700 rounded-md shadow-sm text-sm font-medium text-indigo-700 whitespace-nowrap hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white focus:z-10"
+              className={`${packages === 'ecom' ? 'relative bg-white py-2 px-6 border-indigo-700 rounded-md shadow-sm text-sm font-medium text-indigo-700 whitespace-nowrap hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white focus:z-10' : 'ml-0.5 relative py-2 px-6 border border-transparent rounded-md text-sm font-medium text-indigo-200 whitespace-nowrap hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white focus:z-10'}`}
             >
               Ecommerce
             </button>
             <button
               type="button"
               onClick={() => setPackages('standard')}
-              className="ml-0.5 relative py-2 px-6 border border-transparent rounded-md text-sm font-medium text-indigo-200 whitespace-nowrap hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white focus:z-10"
+              className={`${packages === 'standard' ? 'relative bg-white py-2 px-6 border-indigo-700 rounded-md shadow-sm text-sm font-medium text-indigo-700 whitespace-nowrap hover:bg-indigo-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white focus:z-10' : 'ml-0.5 relative py-2 px-6 border border-transparent rounded-md text-sm font-medium text-indigo-200 whitespace-nowrap hover:bg-indigo-800 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-indigo-700 focus:ring-white focus:z-10'}`}
             >
               Standard
             </button>
@@ -641,337 +641,8 @@ export default function Pricing() {
 
       </div>
 
-      {/* Feature comparison (up to lg) */}
-      <section aria-labelledby="mobile-comparison-heading" className="lg:hidden">
-        <h2 id="mobile-comparison-heading" className="sr-only">
-          Feature comparison
-        </h2>
 
-        <div className="max-w-2xl mx-auto py-16 px-4 space-y-16 sm:px-6">
-          {ecommercePlans.map((plan, mobilePlanIndex) => (
-            <div key="plan.title" className="border-t border-gray-200">
-              <div
-                className={classNames(
-                  plan.featured ? 'border-indigo-600' : 'border-transparent',
-                  '-mt-px pt-6 border-t-2 sm:w-1/2'
-                )}
-              >
-                <h3 className={classNames(plan.featured ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-bold')}>
-                  {plan.title}
-                </h3>
-                <p className="mt-2 text-sm text-gray-500">{plan.description}</p>
-              </div>
-              <h4 className="mt-10 text-sm font-bold text-gray-900">Catered for business</h4>
-
-              <div className="mt-6 relative">
-                {/* Fake card background */}
-                <div aria-hidden="true" className="hidden absolute inset-0 pointer-events-none sm:block">
-                  <div
-                    className={classNames(
-                      plan.featured ? 'shadow-md' : 'shadow',
-                      'absolute right-0 w-1/2 h-full bg-white rounded-lg'
-                    )}
-                  />
-                </div>
-
-                <div
-                  className={classNames(
-                    plan.featured ? 'ring-2 ring-indigo-600 shadow-md' : 'ring-1 ring-black ring-opacity-5 shadow',
-                    'relative py-3 px-4 bg-white rounded-lg sm:p-0 sm:bg-transparent sm:rounded-none sm:ring-0 sm:shadow-none'
-                  )}
-                >
-                  <dl className="divide-y divide-gray-200">
-                    {ecomFeatures.map((feature) => (
-                      <div
-                        key={feature.title}
-                        className="py-3 flex items-center justify-between sm:grid sm:grid-cols-2"
-                      >
-                        <dt className="pr-4 text-sm font-medium text-gray-600">{feature.title}</dt>
-                        <dd className="flex items-center justify-end sm:px-4 sm:justify-center">
-                          {typeof feature.tiers[mobilePlanIndex].value === 'string' ? (
-                            <span
-                              className={classNames(
-                                feature.tiers[mobilePlanIndex].featured ? 'text-indigo-600' : 'text-gray-900',
-                                'text-sm font-medium'
-                              )}
-                            >
-                              {feature.tiers[mobilePlanIndex].value}
-                            </span>
-                          ) : (
-                            <>
-                              {feature.tiers[mobilePlanIndex].value === true ? (
-                                <CheckIcon className="mx-auto h-5 w-5 text-indigo-600" aria-hidden="true" />
-                              ) : (
-                                <XIcon className="mx-auto h-5 w-5 text-gray-400" aria-hidden="true" />
-                              )}
-
-                              <span className="sr-only">
-                                {feature.tiers[mobilePlanIndex].value === true ? 'Yes' : 'No'}
-                              </span>
-                            </>
-                          )}
-                        </dd>
-                      </div>
-                    ))}
-                  </dl>
-                </div>
-
-                {/* Fake card border */}
-                <div aria-hidden="true" className="hidden absolute inset-0 pointer-events-none sm:block">
-                  <div
-                    className={classNames(
-                      plan.featured ? 'ring-2 ring-indigo-600' : 'ring-1 ring-black ring-opacity-5',
-                      'absolute right-0 w-1/2 h-full rounded-lg'
-                    )}
-                  />
-                </div>
-              </div>
-
-              <h4 className="mt-10 text-sm font-bold text-gray-900">Other perks</h4>
-
-              <div className="mt-6 relative">
-                {/* Fake card background */}
-                <div aria-hidden="true" className="hidden absolute inset-0 pointer-events-none sm:block">
-                  <div
-                    className={classNames(
-                      plan.featured ? 'shadow-md' : 'shadow',
-                      'absolute right-0 w-1/2 h-full bg-white rounded-lg'
-                    )}
-                  />
-                </div>
-
-                <div
-                  className={classNames(
-                    plan.featured ? 'ring-2 ring-indigo-600 shadow-md' : 'ring-1 ring-black ring-opacity-5 shadow',
-                    'relative py-3 px-4 bg-white rounded-lg sm:p-0 sm:bg-transparent sm:rounded-none sm:ring-0 sm:shadow-none'
-                  )}
-                >
-                  <dl className="divide-y divide-gray-200">
-                    {ecomPerks.map((perk) => (
-                      <div key={perk.title} className="py-3 flex justify-between sm:grid sm:grid-cols-2">
-                        <dt className="text-sm font-medium text-gray-600 sm:pr-4">{perk.title}</dt>
-                        <dd className="text-center sm:px-4">
-                          {perk.tiers[mobilePlanIndex].value === true ? (
-                            <CheckIcon className="mx-auto h-5 w-5 text-indigo-600" aria-hidden="true" />
-                          ) : (
-                            <XIcon className="mx-auto h-5 w-5 text-gray-400" aria-hidden="true" />
-                          )}
-
-                          <span className="sr-only">{perk.tiers[mobilePlanIndex].value === true ? 'Yes' : 'No'}</span>
-                        </dd>
-                      </div>
-                    ))}
-                  </dl>
-                </div>
-
-                {/* Fake card border */}
-                <div aria-hidden="true" className="hidden absolute inset-0 pointer-events-none sm:block">
-                  <div
-                    className={classNames(
-                      plan.featured ? 'ring-2 ring-indigo-600' : 'ring-1 ring-black ring-opacity-5',
-                      'absolute right-0 w-1/2 h-full rounded-lg'
-                    )}
-                  />
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </section>
-
-      {/* Feature comparison (lg+) */}
-      <section aria-labelledby="comparison-heading" className="hidden lg:block">
-        <h2 id="comparison-heading" className="sr-only">
-          Feature comparison
-        </h2>
-
-        <div className="max-w-7xl mx-auto py-24 px-8">
-          <div className="w-full border-t border-gray-200 flex items-stretch">
-            <div className="-mt-px w-1/4 py-6 pr-4 flex items-end">
-              <h3 className="mt-auto text-sm font-bold text-gray-900">Catered for business</h3>
-            </div>
-            {ecommercePlans.map((plan, planIdx) => (
-              <div
-                key={plan.title}
-                aria-hidden="true"
-                className={classNames(planIdx === ecommercePlans.length - 1 ? '' : 'pr-4', '-mt-px pl-4 w-1/4')}
-              >
-                <div
-                  className={classNames(plan.featured ? 'border-indigo-600' : 'border-transparent', 'py-6 border-t-2')}
-                >
-                  <p className={classNames(plan.featured ? 'text-indigo-600' : 'text-gray-900', 'text-sm font-bold')}>
-                    {plan.title}
-                  </p>
-                  <p className="mt-2 text-sm text-gray-500">{plan.description}</p>
-                </div>
-              </div>
-            ))}
-          </div>
-
-          <div className="relative">
-            {/* Fake card backgrounds */}
-            <div className="absolute inset-0 flex items-stretch pointer-events-none" aria-hidden="true">
-              <div className="w-1/4 pr-4" />
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full bg-white rounded-lg shadow" />
-              </div>
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full bg-white rounded-lg shadow-md" />
-              </div>
-              <div className="w-1/4 pl-4">
-                <div className="w-full h-full bg-white rounded-lg shadow" />
-              </div>
             </div>
 
-            <table className="relative w-full">
-              <caption className="sr-only">Business feature comparison</caption>
-              <thead>
-                <tr className="text-left">
-                  <th scope="col">
-                    <span className="sr-only">Feature</span>
-                  </th>
-                  {ecommercePlans.map((plan) => (
-                    <th key={plan.title} scope="col">
-                      <span className="sr-only">{plan.title} plan</span>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-100">
-                {ecomFeatures.map((feature) => (
-                  <tr key={feature.title}>
-                    <th scope="row" className="w-1/4 py-3 pr-4 text-left text-sm font-medium text-gray-600">
-                      {feature.title}
-                    </th>
-                    {feature.tiers.map((tier, tierIdx) => (
-                      <td
-                        key={tier.title}
-                        className={classNames(
-                          tierIdx === feature.tiers.length - 1 ? 'pl-4' : 'px-4',
-                          'relative w-1/4 py-0 text-center'
-                        )}
-                      >
-                        <span className="relative w-full h-full py-3">
-                          {typeof tier.value === 'string' ? (
-                            <span
-                              className={classNames(
-                                tier.featured ? 'text-indigo-600' : 'text-gray-900',
-                                'text-sm font-medium'
-                              )}
-                            >
-                              {tier.value}
-                            </span>
-                          ) : (
-                            <>
-                              {tier.value === true ? (
-                                <CheckIcon className="mx-auto h-5 w-5 text-indigo-600" aria-hidden="true" />
-                              ) : (
-                                <XIcon className="mx-auto h-5 w-5 text-gray-400" aria-hidden="true" />
-                              )}
-
-                              <span className="sr-only">{tier.value === true ? 'Yes' : 'No'}</span>
-                            </>
-                          )}
-                        </span>
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-
-            {/* Fake card borders */}
-            <div className="absolute inset-0 flex items-stretch pointer-events-none" aria-hidden="true">
-              <div className="w-1/4 pr-4" />
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full rounded-lg ring-1 ring-black ring-opacity-5" />
-              </div>
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full rounded-lg ring-2 ring-indigo-600 ring-opacity-100" />
-              </div>
-              <div className="w-1/4 pl-4">
-                <div className="w-full h-full rounded-lg ring-1 ring-black ring-opacity-5" />
-              </div>
-            </div>
-          </div>
-
-          <h3 className="mt-10 text-sm font-bold text-gray-900">Other perks</h3>
-
-          <div className="mt-6 relative">
-            {/* Fake card backgrounds */}
-            <div className="absolute inset-0 flex items-stretch pointer-events-none" aria-hidden="true">
-              <div className="w-1/4 pr-4" />
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full bg-white rounded-lg shadow" />
-              </div>
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full bg-white rounded-lg shadow-md" />
-              </div>
-              <div className="w-1/4 pl-4">
-                <div className="w-full h-full bg-white rounded-lg shadow" />
-              </div>
-            </div>
-
-            <table className="relative w-full">
-              <caption className="sr-only">Perk comparison</caption>
-              <thead>
-                <tr className="text-left">
-                  <th scope="col">
-                    <span className="sr-only">Perk</span>
-                  </th>
-                  {ecommercePlans.map((plan) => (
-                    <th key={plan.title} scope="col">
-                      <span className="sr-only">{plan.title} plan</span>
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody className="divide-y divide-gray-100">
-                {ecomPerks.map((perk) => (
-                  <tr key={perk.title}>
-                    <th scope="row" className="w-1/4 py-3 pr-4 text-left text-sm font-medium text-gray-600">
-                      {perk.title}
-                    </th>
-                    {perk.tiers.map((tier, tierIdx) => (
-                      <td
-                        key={tier.title}
-                        className={classNames(
-                          tierIdx === perk.tiers.length - 1 ? 'pl-4' : 'px-4',
-                          'relative w-1/4 py-0 text-center'
-                        )}
-                      >
-                        <span className="relative w-full h-full py-3">
-                          {tier.value === true ? (
-                            <CheckIcon className="mx-auto h-5 w-5 text-indigo-600" aria-hidden="true" />
-                          ) : (
-                            <XIcon className="mx-auto h-5 w-5 text-gray-400" aria-hidden="true" />
-                          )}
-
-                          <span className="sr-only">{tier.value === true ? 'Yes' : 'No'}</span>
-                        </span>
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-
-            {/* Fake card borders */}
-            <div className="absolute inset-0 flex items-stretch pointer-events-none" aria-hidden="true">
-              <div className="w-1/4 pr-4" />
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full rounded-lg ring-1 ring-black ring-opacity-5" />
-              </div>
-              <div className="w-1/4 px-4">
-                <div className="w-full h-full rounded-lg ring-2 ring-indigo-600 ring-opacity-100" />
-              </div>
-              <div className="w-1/4 pl-4">
-                <div className="w-full h-full rounded-lg ring-1 ring-black ring-opacity-5" />
-              </div>
-            </div>
-          </div>
-        </div>
-      </section>
-    </div>
   )
 }
