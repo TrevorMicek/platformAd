3ef00b86eb6bb43a6da866ced2c8e32bd7904181
TrevorMicek <miceking@comcast.net>
2022-02-15 08:09:43 -0700

    fixed icon placement on navbar

--- src/components/tailwindComps/Hero.js
+++ src/components/tailwindComps/Hero.js
@@ -72,7 +72,7 @@ export default function HeroComp() {
         >
           <Popover.Panel focus className="absolute z-10 top-0 inset-x-0 p-2 transition transform origin-top md:hidden">
             <div className="rounded-lg shadow-md bg-white ring-1 ring-black ring-opacity-5 overflow-hidden">
-              <div className="h-20 w-auto px-5 pt-4 flex items-center justify-between">
+              <div className="h-20 w-auto  pt-8 flex items-center justify-between">
                 <div>
                   <img
                     className="h-28 w-auto"
