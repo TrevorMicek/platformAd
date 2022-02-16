84f5badf9ff39160dd7571283e25f0b14b083db8
TrevorMicek <miceking@comcast.net>
2022-02-16 16:08:14 -0700

    added prompt for entering email before free tier on pricing page

--- src/components/tailwindComps/Confirmation.js
+++ src/components/tailwindComps/Confirmation.js
@@ -6,14 +6,19 @@ import { CheckIcon } from '@heroicons/react/outline'
 
 export default function Example(props) {
   const [open, setOpen] = useState(true)
+  const [prompt, setPrompt] = useState(true)
   const completeButtonRef = useRef(null)
   const confirm = () => {
         setOpen(false)
        return  props.confirm()
     }
+    const sendForm = (e) => {
+      return e.preventDefault
+    }
   return (
+
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={setOpen}>
+      <Dialog as="div" className="fixed z-10 inset-0 overflow-y-auto" onClose={() => setOpen(false)}>
         <div className="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
           <Transition.Child
             as={Fragment}
@@ -40,32 +45,70 @@ export default function Example(props) {
             leaveFrom="opacity-100 translate-y-0 sm:scale-100"
             leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
           >
-            <div className="relative bottom-32 inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
-              <div>
-                <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
-                  <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
-                </div>
-                <div className="mt-3 text-center sm:mt-5">
-                  <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
-                    Thanks for choosing Websites By Trevor!
-                  </Dialog.Title>
-                  <div className="mt-2">
-                    <p className="text-sm text-gray-500">
-                      We will reach out ASAP to start collaborating on your new free site
-                    </p>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-5 sm:mt-6">
-                <button
-                  type="button"
-                  className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
-                  onClick={confirm}
-                >
-                  Go back to main page
-                </button>
+
+
+            {
+              prompt ? <div className=" relative bottom-32 inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+            <form action={sendForm}  className="mt-12 sm:max-w-xl sm:mx-auto lg:mx-0">
+                      <div className="sm:flex">
+                        <div className="border-2 mb-8 border-gray-500 min-w-0 flex-1">
+                          <label htmlFor="email" className="sr-only">
+                            Email address
+                          </label>
+                          <input
+                            id="email"
+                            type="email"
+                            placeholder="Enter your email"
+                            className="block w-full px-4 py-3 rounded-md border-0 text-base text-bg placeholder-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-bg"
+                          />
+                        </div>
+                        <div className="mt-3 sm:mt-0 sm:ml-3">
+                          <button
+                            type="button"
+                            onClick={() => setPrompt(false)}
+                            className="block w-full py-3 px-4 rounded-md shadow bg-default text-white font-medium hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-300 focus:ring-offset-bg"
+                          >
+                            Start free package
+                          </button>
+                        </div>
+                      </div>
+                      <p className="mt-3 text-sm text-gray-600 sm:mt-4">
+                        Start your free one page website, no credit card necessary. By providing your email, you agree to
+                        our{' '}
+                        <a href="#" className="font-medium text-white">
+                          terms of service
+                        </a>
+                        .
+                      </p>
+                    </form>
+            </div> : <div className="relative bottom-32 inline-block align-bottom bg-white rounded-lg px-4 pt-5 pb-4 text-left overflow-hidden shadow-xl transform transition-all sm:my-8 sm:align-middle sm:max-w-sm sm:w-full sm:p-6">
+            <div>
+            <div className="mx-auto flex items-center justify-center h-12 w-12 rounded-full bg-green-100">
+              <CheckIcon className="h-6 w-6 text-green-600" aria-hidden="true" />
+            </div>
+            <div className="mt-3 text-center sm:mt-5">
+              <Dialog.Title as="h3" className="text-lg leading-6 font-medium text-gray-900">
+                Thanks for choosing Websites By Trevor!
+              </Dialog.Title>
+              <div className="mt-2">
+                <p className="text-sm text-gray-500">
+                  We will reach out ASAP to start collaborating on your new free site
+                </p>
               </div>
             </div>
+          </div>
+          <div className="mt-5 sm:mt-6">
+            <button
+              type="button"
+              className="inline-flex justify-center w-full rounded-md border border-transparent shadow-sm px-4 py-2 bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm"
+              onClick={confirm}
+            >
+              Go back to main page
+            </button>
+          </div>
+        </div>
+            }
+
           </Transition.Child>
         </div>
       </Dialog>
