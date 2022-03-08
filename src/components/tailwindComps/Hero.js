b29f6f1bfabdb5401fab40e0f86d118cda5a0337
TrevorMicek <miceking@comcast.net>
2022-03-08 10:09:09 -0700

    fixed get a site button

--- src/components/tailwindComps/Hero.js
+++ src/components/tailwindComps/Hero.js
@@ -146,15 +146,16 @@ export default function HeroComp() {
 
 
             <div className="sm:col-span-2">
-              <button>
-              <Link to="/"
+
+              <a
+                href="/"
                 type="submit"
                 onClick={() => setConfirm(true)}
                 className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
                 Get a site now
-              </Link>
-              </button>
+              </a>
+
             </div>
 
 
