aaf1ac2b456b852b4ec116d90a77cd52f64c553e
TrevorMicek <miceking@comcast.net>
2022-03-08 10:25:05 -0700

    trying to fix header button looking weird on mobile

--- src/components/tailwindComps/Hero.js
+++ src/components/tailwindComps/Hero.js
@@ -145,16 +145,21 @@ export default function HeroComp() {
                   <div className="mt-10 sm:mt-12">
 
 
-            <div className="sm:col-span-2">
 
-              <a
-                href="/"
-                type="submit"
-                onClick={() => setConfirm(true)}
-                className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+
+
+
+
+
+            <div className="sm:col-span-2">
+            <button
+
+              type="submit"
+              onClick={() => setConfirm(true)}
+              className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-                Get a site now
-              </a>
+              Get a site now
+              </button>
 
             </div>
 
