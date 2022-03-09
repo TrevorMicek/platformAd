71a3e056a86a6e66977d2e6b16b81301cf07a650
TrevorMicek <miceking@comcast.net>
2022-03-08 20:02:25 -0700

    fix for site button padding

--- src/components/tailwindComps/Hero.js
+++ src/components/tailwindComps/Hero.js
@@ -156,9 +156,9 @@ export default function HeroComp() {
 
               type="submit"
               onClick={() => setConfirm(true)}
-              className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+              className="w-full inline-flex items-center justify-center  border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
               >
-              <Link to="/pricing">Get a site now</Link>
+              <Link to="/pricing" className="w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Get a site now</Link>
               </button>
 
             </div>
