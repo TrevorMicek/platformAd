a16b18df1fed87b834c2404268f87d98a60740ad
TrevorMicek <miceking@comcast.net>
2022-03-11 16:32:21 -0700

    using cloundinary for bg img

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 import MainHeader from '../tailwindComps/Hero'
 import SecondaryHeader from '../tailwindComps/SecondaryHeader'
-import PartnerHeader from '../tailwindComps/partner/Hero'
+
 const Header = (props) => {
 
   const firstTitle = 'Small Business Web Design & Development'
@@ -46,8 +46,7 @@ const Header = (props) => {
           return <MainHeader />
         case secondTitle:
           return <SecondaryHeader title={props.title} text={props.text} />
-        case 'partnerships':
-          return <PartnerHeader title={props.title} text={props.text} />
+
         default:
           return <StoreHeader />
     }
